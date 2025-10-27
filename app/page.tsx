b54f219b120cff0a52d89840aceb6e6e0dd20dcd
NightW0lfff <philong170101@gmail.com>
2025-10-28 03:06:32 +1100

    Add Humidity bar chart to the page and styling the current weather section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { data } from "@/app/data/dummy-5days";
+import HumidityBarChart from "@/components/humidityBarChart";
 import TempLineChart from "@/components/tempLineChart";
 
 export default function Home() {
@@ -6,23 +7,27 @@ export default function Home() {
 
 	return (
 		<div className="min-h-dvh w-dvw max-w-[1440px] m-auto">
-			<main className="flex flex-col p-2 h-dvh item gap-2">
+			<main className="flex flex-col p-4 h-dvh item gap-2">
 				{/* Current Weather */}
-				<section className="flex flex-col items-center justify-center w-full border-2 p-4 border-accent-foreground/60 rounded-2xl">
-					<div className="flex flex-col w-full">
-						<div className="flex justify-around w-full">
-							<span>30dg</span>
-							<span>21 December 2025</span>
+				<section className="flex flex-col items-center justify-center w-full border-2 p-6 border-accent-foreground/60 rounded-2xl gap-4">
+					<div className="flex flex-col w-full gap-4">
+						<div className="flex justify-between items-center w-full p-4">
+							<span className="text-7xl font-semibold p-4">30dg</span>
+							<span className=" text-4xl">21 December 2025</span>
 						</div>
 
-						<div className="flex justify-between w-full overflow-hidden ">
+						<div className="flex justify-start w-full overflow-hidden p-4 border-2 rounded-xl">
 							{DATA.map((data) => {
 								return (
-									<div key={data.dt} className="flex flex-col items-center">
-										<span>{data.main.temp}</span>
-										<span className="">
+									<div
+										key={data.dt}
+										className="flex flex-col grow not-last:border-r items-center font-semibold text-xl gap-4 p-4"
+									>
+										<span className="">{Math.round(data.main.temp)}&deg;C</span>
+										<span className="text-sm text-muted-foreground">
 											{new Date(data.dt * 1000).toLocaleTimeString(["en-AU"], {
 												hour: "2-digit",
+												minute: "2-digit",
 												timeZone: "UTC",
 											})}
 										</span>
@@ -35,74 +40,46 @@ export default function Home() {
 						<TempLineChart data={data} />
 					</div>
 				</section>
-				<section className="w-full border-2 p-4 border-accent-foreground/60 rounded-2xl">
-					<div className="flex w-full">
-						<div className="flex flex-col justify-center border-2 rounded-xl items-center p-4">
-							<h3>5 Days Forecast</h3>
-							<div className="flex">
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
+				<section className="w-full border-2 p-6 border-accent-foreground/60 rounded-2xl">
+					<div className="flex w-full gap-6">
+						<div className="flex flex-col justify-start border-2 rounded-xl gap-4 items-center p-4 h-fit">
+							<h3 className="font-semibold">5 Days Forecast</h3>
+							<div className="grid grid-cols-3 auto-rows-auto w-full text-center gap-y-2">
+								<span className="not-last:border-r border-accent-foreground/70 px-4 ">
 									30/1
 								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
+								<span className="not-last:border-r border-accent-foreground/70 px-4">
 									30
 								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									18
-								</span>
-							</div>
-							<div className="flex">
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
+								<span className="px-4">18</span>
+								<span className="not-last:border-r border-accent-foreground/70 px-4">
 									30/1
 								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
+								<span className="not-last:border-r border-accent-foreground/70 px-4">
 									30
 								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									18
-								</span>
-							</div>
-							<div className="flex">
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30/1
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									18
-								</span>
-							</div>
-							<div className="flex">
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30/1
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									18
-								</span>
-							</div>
-							<div className="flex">
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30/1
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									30
-								</span>
-								<span className="not-last:border-r border-accent-foreground/70 px-2">
-									18
-								</span>
+								<span className="px-4">18</span>
 							</div>
 						</div>
-						<div className="grow flex flex-col justify-center items-center">
-							<div>Rain</div>
-							<div className="flex gap-2">
-								<div>Humidity</div>
-								<div>Wind Speed</div>
-								<div>Compass</div>
+						<div className="grow flex flex-col justify-center items-center gap-6">
+							<HumidityBarChart />
+							<div className="grid grid-cols-3 text-center w-full gap-x-4">
+								<div className=" border-2 rounded-xl flex flex-col">
+									<h3 className="font-semibold border-b p-4">Humidity</h3>
+									<div className="p-4 text-4xl tracking-wide">30</div>
+								</div>
+								<div className=" border-2 rounded-xl flex flex-col">
+									<h3 className="font-semibold border-b p-4">Wind Speed</h3>
+									<div className="p-4 text-4xl tracking-wide">30</div>
+								</div>
+								<div className=" border-2 rounded-xl flex flex-col">
+									<h3 className="font-semibold border-b p-4">Compass</h3>
+									<div className="p-4 text-4xl tracking-wide">123</div>
+								</div>
 							</div>
-							<div>Location Map Embed</div>
+							<div className=" h-[200px] w-full border-2 rounded-xl">
+								Location Map Embed
+							</div>
 						</div>
 					</div>
 				</section>
---
+++ components/humidityBarChart.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
+
+import {
+	ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "./ui/chart";
+import { data } from "@/app/data/dummy-5days";
+
+function HumidityBarChart() {
+	const chartData = data.listX.slice(0, 13).map((item) => {
+		return {
+			hr: new Date(item.dt * 1000).toLocaleTimeString("en-AU", {
+				hour: "2-digit",
+				timeZone: "UTC",
+				hour12: false,
+			}),
+			humidity: item.main.humidity,
+		};
+	});
+
+	const chartConfig = {
+		humidity: {
+			label: "Humidity",
+			color: "var(--chart-2)",
+		},
+	} satisfies ChartConfig;
+
+	return (
+		<ChartContainer
+			config={chartConfig}
+			className="w-full h-[250px] border-2 rounded-xl"
+		>
+			<BarChart accessibilityLayer data={chartData} margin={{ top: 20 }}>
+				<XAxis
+					dataKey={"hr"}
+					tickLine={false}
+					axisLine={false}
+					tickMargin={8}
+					tickFormatter={(value) => value.slice(0, 3)}
+				/>
+				<CartesianGrid vertical={false} />
+				<Bar dataKey="humidity" fill="var(--color-humidity)" radius={4}>
+					<LabelList
+						dataKey={"humidity"}
+						position={"top"}
+						offset={8}
+						fontSize={12}
+						className="fill-foreground"
+					/>
+				</Bar>
+				<ChartTooltip
+					content={<ChartTooltipContent hideLabel indicator="line" />}
+					cursor={false}
+				/>
+			</BarChart>
+		</ChartContainer>
+	);
+}
+
+export default HumidityBarChart;
