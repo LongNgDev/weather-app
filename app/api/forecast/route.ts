636731e6ce7809cff842da46c28587e1e03fce4c
NightW0lfff <philong170101@gmail.com>
2025-12-29 18:28:22 +1100

    bug: Remove redis cache due to the database will auto delete the storage on upstash if inactive for more than 2 weeks. Back to revalidate from Next.

--- app/api/forecast/route.ts
+++ app/api/forecast/route.ts
@@ -1,30 +1,31 @@
 "use server";
 
 import { WeatherData } from "@/app/data/dummy-5days";
-import { Redis } from "@upstash/redis";
+// import { Redis } from "@upstash/redis";
 import { NextResponse } from "next/server";
 
-const redis = Redis.fromEnv();
+/* const redis = Redis.fromEnv();
 
 const TTL = 300; // 300 for 5mins
-
+ */
 export async function GET(req: Request) {
 	const { searchParams } = new URL(req.url);
 
 	const lat = searchParams.get("lat");
 	const lon = searchParams.get("lon");
-	const key = `${lat}:${lon}`;
+	// const key = `${lat}:${lon}`;
 	const appid = process.env.WEATHER_KEY;
 
 	if (!lat || !lon)
 		return NextResponse.json({ error: "Missing lat/lon" }, { status: 400 });
 
 	// Check cache
-	const cached = await redis.get(key);
-	if (cached) return NextResponse.json(cached);
+	/* 	const cached = await redis.get(key);
+	if (cached) return NextResponse.json(cached); */
 
 	const res = await fetch(
-		`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`
+		`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`,
+		{ next: { revalidate: 300 } }
 	);
 
 	if (!res.ok)
@@ -32,7 +33,7 @@ export async function GET(req: Request) {
 
 	const data: WeatherData = await res.json();
 
-	await redis.setex(key, TTL, data);
+	// await redis.setex(key, TTL, data);
 
 	return NextResponse.json(data);
 }
